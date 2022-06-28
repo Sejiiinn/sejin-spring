1b013176ff894bd93d4b57d8d35bc6d25230577f
sejin <rlatpwlsdlek@gmail.com>
2022-06-28 17:52:57 +0900

    init

---
+++ src/main/java/sejin/sejinspring/SejinSpringApplication.java
@@ -0,0 +1,13 @@
+package sejin.sejinspring;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SejinSpringApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SejinSpringApplication.class, args);
+	}
+
+}
---
+++ src/test/java/sejin/sejinspring/SejinSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package sejin.sejinspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SejinSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
