619e4b4b12e9441bebd0dab37d711802550aa92f
sejin <rlatpwlsdlek@gmail.com>
2022-06-29 16:30:59 +0900

    HomePage & registrationPage

--- src/main/java/sejin/sejinspring/controller/MemberController.java
+++ src/main/java/sejin/sejinspring/controller/MemberController.java
@@ -2,14 +2,33 @@ package sejin.sejinspring.controller;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import sejin.sejinspring.domain.Member;
 import sejin.sejinspring.service.MemberService;
 
 @Controller
 public class MemberController {
-    private final MemberService memberService;
+
+    private MemberService memberService;
 
     @Autowired
     public MemberController(MemberService memberService) {
         this.memberService = memberService;
+    }
+
+    @GetMapping("/members/new")
+    public String createForm() {
+        return "members/createMemberForm";
+    }
+
+    @PostMapping("/members/new")
+    public String create(MemberForm form) {
+        Member member = new Member();
+        member.setName(form.getName());
+
+        memberService.join(member);
+
+        return "redirect:/";
     }
 }
