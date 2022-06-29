71adab84e8a81ab89c3bf16403d0238c6b966a05
sejin <rlatpwlsdlek@gmail.com>
2022-06-29 15:33:41 +0900

    Spring Bean, @Bean

---
+++ src/main/java/sejin/sejinspring/SpringConfig.java
@@ -0,0 +1,21 @@
+package sejin.sejinspring;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import sejin.sejinspring.repository.MemberRepository;
+import sejin.sejinspring.repository.MemoryMemberRepository;
+import sejin.sejinspring.service.MemberService;
+
+@Configuration
+public class SpringConfig {
+
+    @Bean
+    public MemberService memberService() {
+        return new MemberService(memberRepository());
+    }
+
+    @Bean
+    public MemberRepository memberRepository() {
+        return new MemoryMemberRepository();
+    }
+}
