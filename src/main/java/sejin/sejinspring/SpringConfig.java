509f45d39a50e0b5367e56d04d11baab09f74182
sejin <rlatpwlsdlek@gmail.com>
2022-06-30 21:07:07 +0900

    JDBC

--- src/main/java/sejin/sejinspring/SpringConfig.java
+++ src/main/java/sejin/sejinspring/SpringConfig.java
@@ -1,13 +1,25 @@
 package sejin.sejinspring;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import sejin.sejinspring.repository.MemberRepository;
 import sejin.sejinspring.repository.MemoryMemberRepository;
+import sejin.sejinspring.repository.JdbcMemberRepository;
 import sejin.sejinspring.service.MemberService;
+
+import javax.sql.DataSource;
+import javax.xml.crypto.Data;
 
 @Configuration
 public class SpringConfig {
+
+    private DataSource dataSource;
+
+    @Autowired
+    public SpringConfig(DataSource dataSource){
+        this.dataSource = dataSource;
+    }
 
     @Bean
     public MemberService memberService() {
@@ -16,6 +28,7 @@ public class SpringConfig {
 
     @Bean
     public MemberRepository memberRepository() {
-        return new MemoryMemberRepository();
+        // return new MemoryMemberRepository();
+        return new JdbcMemberRepository(dataSource);
     }
 }
